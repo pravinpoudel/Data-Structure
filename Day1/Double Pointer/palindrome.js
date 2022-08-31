bd57be9b1d743bfae218dfc33822a836983272ef
pravinpoudel <prvnpoudel4@gmail.com>
2022-08-31 16:22:56 -0600

    case insenstive test case addedd

--- Day1/Double Pointer/palindrome.js
+++ Day1/Double Pointer/palindrome.js
@@ -2,7 +2,9 @@ function isPalindrome(inputString) {
   let pointer0 = 0;
   let pointer1 = inputString.length - 1;
   while (pointer1 > pointer0) {
-    if (inputString[pointer0] != inputString[pointer1]) {
+    if (
+      inputString[pointer0].toLowerCase() != inputString[pointer1].toLowerCase()
+    ) {
       return false;
     }
     pointer0++;
@@ -11,4 +13,4 @@ function isPalindrome(inputString) {
   return true;
 }
 
-console.log(isPalindrome("daddy"));
+console.log(isPalindrome("Rotator"));
